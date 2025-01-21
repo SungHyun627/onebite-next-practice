cf8a2cde6ba852fb62cfaabb1f06a9a5ec3ae8ac
Sunghyun627 <kksscc4@gmail.com>
2025-01-21 17:46:06 +0900

    feat: 한입 북스 UI 구현

--- section03/src/components/client-component.tsx
+++
@@ -1,8 +0,0 @@
-'use Client';
-
-import { ReactNode } from 'react';
-
-export default function ClientComponent({ children }: { children: ReactNode }) {
-  console.log('클라이언트');
-  return <div>{children}</div>;
-}
