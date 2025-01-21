5b56efad4b83c98dba9b5491cdaff5b6a73b056d
Sunghyun627 <kksscc4@gmail.com>
2025-01-21 18:18:02 +0900

    feat: 추천 도서 가져오는 로직 구현

--- section04/src/app/(with-searchbar)/page.tsx
+++ section04/src/app/(with-searchbar)/page.tsx
@@ -20,14 +20,29 @@ async function AllBooks() {
   );
 }
 
+async function REcoBooks() {
+  const response = await fetch(`http://localhost:12345/book/random`);
+  if (!response.ok) {
+    return <div>오류가 발생했습니다...</div>;
+  }
+  const recoBooks: BookData[] = await response.json();
+  console.log(recoBooks);
+
+  return (
+    <div>
+      {recoBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
+
 export default async function Home() {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
+        <REcoBooks />
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
