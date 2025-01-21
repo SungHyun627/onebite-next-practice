bf1e48cd28796ec8922de70442c8bf11fa4fd1b4
Sunghyun627 <kksscc4@gmail.com>
2025-01-21 18:16:13 +0900

    feat: 전체 도서 목록 가져오는 로직 구현

--- section04/src/app/(with-searchbar)/page.tsx
+++ section04/src/app/(with-searchbar)/page.tsx
@@ -1,8 +1,26 @@
-import BookItem from "@/components/book-item";
-import style from "./page.module.css";
-import books from "@/mock/books.json";
+import BookItem from '@/components/book-item';
+import style from './page.module.css';
+import books from '@/mock/books.json';
+import { BookData } from '@/types';
 
-export default function Home() {
+async function AllBooks() {
+  const response = await fetch(`http://localhost:12345/book`);
+  if (!response.ok) {
+    return <div>오류가 발생했습니다...</div>;
+  }
+  const allBooks: BookData[] = await response.json();
+  console.log(allBooks);
+
+  return (
+    <div>
+      {allBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
+
+export default async function Home() {
   return (
     <div className={style.container}>
       <section>
@@ -13,9 +31,7 @@ export default function Home() {
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
+        <AllBooks />
       </section>
     </div>
   );
