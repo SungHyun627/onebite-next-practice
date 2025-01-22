42957576833c8b6cc7cead6a7e059377c99b8226
Sunghyun627 <kksscc4@gmail.com>
2025-01-22 16:49:34 +0900

    feat: index 페이지에 skeleton UI 적용

--- section06/src/app/(with-searchbar)/page.tsx
+++ section06/src/app/(with-searchbar)/page.tsx
@@ -1,9 +1,9 @@
 import BookItem from '@/components/book-item';
 import style from './page.module.css';
-import books from '@/mock/books.json';
 import { BookData } from '@/types';
 import { delay } from '@/util/delay';
 import { Suspense } from 'react';
+import BookListSkeleton from '@/components/skeleton/book-list-skeleton';
 
 async function AllBooks() {
   await delay(1500);
@@ -50,13 +50,14 @@ export default async function Home() {
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        <Suspense fallback={<div>도서를 불러오는 중입니다....</div>}>
+
+        <Suspense fallback={<BookListSkeleton count={3} />}>
           <RecoBooks />
         </Suspense>
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        <Suspense fallback={<div>도서를 불러오는 중입니다....</div>}>
+        <Suspense fallback={<BookListSkeleton count={3} />}>
           <AllBooks />
         </Suspense>
       </section>
